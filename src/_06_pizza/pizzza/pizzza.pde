1543ee76a9c4d98ab6cdb59e88b4835b796f9c24
league <league@imac-17.attlocal.net>
2019-09-23 19:58:28 -0700

    riddLES

--- src/_06_pizza/pizzza/pizzza.pde
+++ src/_06_pizza/pizzza/pizzza.pde
@@ -1,4 +1,10 @@
+  import ddf.minim.*;  
+
+
+Minim minim;     //at the top of the sketch
+AudioPlayer sound; 
 void setup(){
+   
       size(1000,1000);
 fill(132,97,96);
 ellipse(500,500,400,400);
@@ -6,10 +12,15 @@ fill(#FF0533);
 ellipse(500,500,350,350);
 fill(#FFC905);
 ellipse(500,500,335,335);
-
+minim = new Minim(this);     
+sound = minim.loadFile("ding.wav");
     }
 
     void draw(){
       PImage vegetables = loadImage("vegetables.png");
-image(vegetables, 350, 350);
+      if(mousePressed){
+image(vegetables, mouseX, mouseY);
+}
+sound.play();
+sound.rewind();
     }
--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,7 +1,7 @@
 package _07_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
-
+import javax.swing.JOptionPane;
 
 
 public class TheRiddler {
@@ -9,17 +9,39 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+			String riddleone = JOptionPane.showInputDialog("A hamburger walked into a bar. Why wasn't he served?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if(riddleone.equals("We don't serve food here.")) 
+		{
+			score+=1;
+		}
+		else
+		{
+		JOptionPane.showMessageDialog(null, "ur bad");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+		String riddletwo = JOptionPane.showInputDialog("What goes up but never comes down?");
 		
+		if(riddleone.equals("A rocket ship ")) 
+		{
+			score+=1;
+		}
+		else
+		{
+		JOptionPane.showMessageDialog(null, "ur bad");
+			}
+		String riddlethree = JOptionPane.showInputDialog("What is 3 + 3?");
+		
+		if(riddleone.equals("69420")) 
+		{
+			score+=1;
+		}
+		else
+		{
+		JOptionPane.showMessageDialog(null, "ur bad");
+			}
+		}
 	}
-}
 
